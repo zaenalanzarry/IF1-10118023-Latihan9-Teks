620ecfb5e9eb33eb5a27a57d7c4e9ebeec41eeea
Zaenal Anzarry <55571352+zaenalanzarry@users.noreply.github.com>
2019-09-25 14:49:59 +0700

    Add files via upload

---
+++ IF1-10118023-Latihan9-Teks/src/latihan9/Latihan9.java
@@ -0,0 +1,27 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package latihan9;
+
+/**
+ * @author Zaenal PC
+ * NAMA              : Zaenal Anzarry
+ * KELAS             : IF-01
+ * NIM               : 10118023
+ * Deskripsi Program : Teks
+ */
+
+public class Latihan9 {
+
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "char:" + tab + "berisi " + c);
+    }
+    
+}
